cf3a8e9cfee7fb48725cda5ea89f97105254e981
MikeRedwood <75983563+MikeRedwood@users.noreply.github.com>
2020-12-16 12:09:29 +0000

    Basic Anvil v1.0

---
+++ init.lua
@@ -0,0 +1,120 @@
+
+minetest.register_craftitem("basic_anvil:anvil", {
+  description = "Tool Repair Anvil",
+  inventory_image = "basic_anvil_image.png",
+})
+
+--Anvil
+
+minetest.register_craft({
+    output = "basic_anvil:anvil",
+    recipe = {{"default:steel_ingot", "default:steelblock", "default:steel_ingot"}},
+})
+
+--Steel
+
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:pick_steel",
+    recipe = {"default:steel_ingot", "default:pick_steel", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:axe_steel",
+    recipe = {"default:steel_ingot", "default:axe_steel", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:shovel_steel",
+    recipe = {"default:steel_ingot", "default:shovel_steel", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:sword_steel",
+    recipe = {"default:steel_ingot", "default:sword_steel", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+
+--Diamond
+
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:pick_diamond",
+    recipe = {"default:diamond", "default:pick_diamond", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:axe_diamond",
+    recipe = {"default:diamond", "default:axe_diamond", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:shovel_diamond",
+    recipe = {"default:diamond", "default:shovel_diamond", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:sword_diamond",
+    recipe = {"default:diamond", "default:sword_diamond", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+
+--Mese
+
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:pick_mese",
+    recipe = {"default:mese_crystal", "default:pick_mese", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:axe_mese",
+    recipe = {"default:mese_crystal", "default:axe_mese", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:shovel_mese",
+    recipe = {"default:mese_crystal", "default:shovel_mese", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:sword_mese",
+    recipe = {"default:mese_crystal", "default:sword_mese", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+
+--Bronze
+
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:pick_bronze",
+    recipe = {"default:bronze_ingot", "default:pick_bronze", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:axe_bronze",
+    recipe = {"default:bronze_ingot", "default:axe_bronze", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:shovel_bronze",
+    recipe = {"default:bronze_ingot", "default:shovel_bronze", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
+minetest.register_craft({
+    type = "shapeless",
+    output = "default:sword_bronze",
+    recipe = {"default:bronze_ingot", "default:sword_bronze", "basic_anvil:anvil"},
+	replacements = {{"basic_anvil:anvil", "basic_anvil:anvil"}},
+})
